8698dc01ef05ed13ccfd256ea84c4569c214f659
Hung Do <hung.do@dominionenterprises.com>
2017-08-22 13:06:56 -0400

    add try catch

--- load-access-log.php
+++ load-access-log.php
@@ -2,8 +2,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 $parser = new \Kassner\LogParser\LogParser();
 
-$accesslogFile = 'www.commercialtrucktrader.com.access.log';
-$base_uri = 'http://www.commercialtrucktrader.com';
+$accesslogFile = 'www.equipmenttrader.com.access.log.1';
+$base_uri = 'http://cloud.equip.local';
 
 
 $parser->setFormat('%h %v %u %a - - -  - - %t "%r" %>s %I %T "%{Referer}i" "%{User-Agent}i"');
@@ -15,11 +15,15 @@ $client = new GuzzleHttp\Client(['base_uri' => $base_uri]);
 foreach ($lines as $line) {
   $entry = $parser->parse($line);
   $pagePath = explode(" ",$entry->request)[1];
+  try {
   $response = $client->request('GET',$pagePath);
   $code = $response->getStatusCode();
   $log = print_r($code." ".$pagePath."\n", true);
   file_put_contents('/tmp/loadtest-access-log.log', $log, FILE_APPEND);
   echo $log;
+  } catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+  }
 }
 
 
